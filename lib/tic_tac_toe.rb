4b63f42297d038c6c21d1f569f27a259f3a58c17
Josh Morris <jmo0624@gmail.com>
2019-04-24 16:49:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -105,14 +105,14 @@ WIN_COMBINATION = [
   
   def won?(board)
     
-     WIN_COMBINATIONS.each {|win_combo|
+    WIN_COMBINATIONS.each {|win_combo|
     
-    index_0 = win_combo[0]
+     index_0 = win_combo[0]
     
-    index_1 = win_combo[1]
+     index_1 = win_combo[1]
     
-    index_2 = win_combo[2]
-
+     index_2 = win_combo[2]
+  
     position_1 = board[index_0]
     
     position_2 = board[index_1]
